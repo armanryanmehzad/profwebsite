abd584eda68e487206ca31850088b9e31f4b81a7
armanryanmehzad <armanryanmehzad@gmail.com>
2025-06-08 21:03:15 -0400

    changes

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import STATIC_ROOT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,6 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['www.armanmehzad.com']
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 
 # Application definition
 
