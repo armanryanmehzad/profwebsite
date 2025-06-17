13fab0b13ba2c45114631fe6fa4834634ce712cd
armanryanmehzad <armanryanmehzad@gmail.com>
2025-06-16 21:17:11 -0400

    Responsive CSS and main page image addition.

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('research/', views.research_page, name='research_page')
+    path('research/', views.research_page, name='research_page'),
+    path('publications/', views.publications_page, name='publications_page'),
+    path('cv/', views.cv_page, name='cv_page'),
+    path('contact/', views.contact_page, name='contact_page')
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@
 # ------------------------------
 from django.shortcuts import render
 from django.http import JsonResponse
+from plotly.graph_objs.layout.shape import Label
+
+
 # ------------------------------
 
 # Create your views here.
@@ -9,14 +12,14 @@ def home(request):
     return render(request, 'main/home.html')
 
 def research_page(request):
-    # Get selected ROIs, defaults to all ROIs
-    # Generate graph for selected ROIs
-    #plot_html = generate_multi_roi_graph(selected_rois)
-
-    # Check if it's an AJAX request (used for updating dynamically)
-    #if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-    #    return JsonResponse({'updated_graph_html': plot_html})
-
-    # Render the full template if not an AJAX request
     return render(request, 'main/research.html')
     #return render(request, 'main/research.html', {'plot_html': plot_html, 'rois': ROIs, 'selected_rois': selected_rois})
+
+def publications_page(request):
+    return render(request, 'main/publications.html')
+
+def cv_page(request):
+    return render(request, 'main/cv.html')
+
+def contact_page(request):
+    return render(request, 'main/contact.html')
